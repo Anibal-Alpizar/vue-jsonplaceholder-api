4b0c07c53453e94eb4c699d1e4008ab01cfdd0d9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-11 19:58:19 -0600

    api fetch

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <h1>Hello World</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>User</th>
+      </tr>
+      <tbody>
+          <tr v-for="user in users" :key="user.id"> 
+              {{ user.name }}
+          </tr>
+      </tbody>
+    </thead>
+  </table>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    console.log(data);
+    this.users = data;
+  },
 };
 </script>
 
